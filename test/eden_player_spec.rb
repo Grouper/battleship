d737e7d7e2374146b0bc41fdadd0b08c4f565802
Ilya Kavalerov <ilya@artsymail.com>
2014-02-08 23:45:50 -0500

    List requirements in specs

--- test/eden_player_spec.rb
+++ test/eden_player_spec.rb
@@ -8,4 +8,43 @@ describe 'EdenPlayer' do
     name.must_equal "Eden"
   end
 
+  describe '#lay_ship' do
+    describe 'generally, without a filter block' do
+      it 'does not suggest a ship outside of the board range'
+      it 'respects a limit parameter'
+    end
+    describe 'with a filter block' do
+      it 'respects inclusion filters'
+      it 'respects exclusion filters'
+      it 'respects both filters simultaneously'
+      it 'generates all possible combinations'
+    end
+  end
+
+  describe '#position_aggregator' do
+    it 'correctly aggregates the possible orientations from #lay_ship'
+  end
+
+  describe 'Offense' do
+    it 'is more likely to choose a central spot early on'
+
+    describe 'the parity rule' do
+      it 'will obey the parity rule when in hunt mode'
+      it 'will update the parity rule when ships are sunk'
+    end
+
+    describe 'responding to a hit' do
+      it 'enters target mode'
+    end
+
+    describe 'responding to a sunk ship' do
+      it 'reenters hunt mode'
+    end
+
+    describe "#next_target's suggestions" do
+      it 'are within a plus shape of the hit'
+      it 'are within the range of the longest ship available'
+    end
+  end
+
 end
